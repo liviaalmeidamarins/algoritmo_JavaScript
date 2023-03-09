5ba5bd9ffe81391e42f17d403b6dcbc52c3134ad
Lívia Almeida Marins <lialmeidamarins@gmail.com>
2023-03-09 20:37:29 -0300

    Add files via upload

---
+++ aula_06/index.js
@@ -0,0 +1,7 @@
+// criando a função
+function escrevendoTexto(nome,sobrenome,idade){
+    console.log(`O nome digitado foi: ${nome} ${sobrenome} e sua idade é ${idade} anos.`)
+}
+escrevendoTexto('Lívia', "Marins", 19)
+escrevendoTexto('Alicheli', "De Almeida", 37)
+escrevendoTexto('Valmir', "Francisco", 19)
