82f206e26e0d28d0f223b65cb08528c8b91a6849
Lívia Almeida Marins <lialmeidamarins@gmail.com>
2023-03-09 12:20:21 -0300

    Add files via upload

---
+++ aula_04/exercicio-06/index.js
@@ -0,0 +1,18 @@
+let numero1 = prompt('')
+let numero2 = prompt('')
+
+numero1 = parseFloat(numero1) // conversão
+numero2 = parseFloat(numero2)
+
+let soma = numero1 + numero2
+let sub = numero1 - numero1
+let mult = numero1 * numero2
+let div = numero1 / numero2
+let mod = numero1 % numero2
+let media = (numero1 + numero2) /2
+
+document.write(`Os números digitados foram: ${numero1} e ${numero2}.<br>`)
+
+document.write(`A soma dos números: ${numero1} e ${numero2} é ${soma}.<br>`)
+
+document.write(`A soma dos números: ${numero1} e ${numero2} é ${numero1 + numero2}.<br>`)
